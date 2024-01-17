97b5f0de22fc6b8361d96c4cf02428947fc418df
Daniel Prandzhev <daniel.prandzhev@gmail.com>
2024-01-17 21:40:56 +0200

    upload

---
+++ JS-Basics/circle-area.js
@@ -0,0 +1,18 @@
+function solve (input) 
+{
+    `use strict`;
+    let result = 0
+
+    if (typeof input === 'number')
+    {
+        result = Math.PI * (input ** 2)
+        console.log(result.toFixed(2));
+    }
+
+    else 
+    {
+        console.log(`We can not calculate the circle area, because we received a ${typeof(input)}.`);
+    }
+}
+
+solve(5);
---
+++ JS-Basics/numbers-from-m-to-n.js
@@ -0,0 +1,9 @@
+function solve(number_m, number_n) 
+{
+    'use strict';
+    for (let i = number_m; i >= number_n; i--) {
+        console.log(i);
+    }
+}
+
+solve(6, 1);
